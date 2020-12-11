76c07dfcc4cf4c11beca8d9f0209a6806ac545fb
BenoitABZ <benoit.abouzeid@gmail.com>
2020-12-11 16:11:54 +0100

    set up eureka

---
+++ giga-eureka/src/test/java/com/gigaboutique/gigaeureka/GigaEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigaboutique.gigaeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigaEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
