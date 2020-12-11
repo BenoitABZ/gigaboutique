e94713d67054f215a03aead549f2a23de241f8b7
BenoitABZ <benoit.abouzeid@gmail.com>
2020-12-11 13:44:16 +0100

    set up userservice and configserver

---
+++ giga-configserver/src/main/java/com/gigaboutique/gigaconfigserver/GigaConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.gigaboutique.gigaconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigaConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigaConfigserverApplication.class, args);
+	}
+
+}
---
+++ giga-configserver/src/test/java/com/gigaboutique/gigaconfigserver/GigaConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigaboutique.gigaconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigaConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
